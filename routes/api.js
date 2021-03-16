e1378f19d09aa5e198a57270bc900b38588a4bd4
appocalyp <77102650+appocalyp@users.noreply.github.com>
2021-03-16 22:58:15 +0700

    api.js

--- routes/api.js
+++ routes/api.js
@@ -153,10 +153,10 @@ var len = 15
             randomlagi += arr[Math.floor(Math.random() * arr.length)];
         }
 
-        var randomTextNumber = random+randomlagi+'---------ArdyyGanteng'+'Ardyy--GANS';
+        var randomTextNumber = random+randomlagi+'---------ArdyyGanteng'+'ARDYY--GANS';
         
  
- async function cekApiKey(api)
+ async function cekApiKey(api) {
  	ap = await zahirr.findOne({apikey:api})
  return ap;
  }
